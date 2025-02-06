df47e2f72e5d217b008aae701da119d996963081
Carinalak <carina.lakosil@gmail.com>
2025-02-06 12:26:09 +0100

    :bug: fix bug

--- src/pages/Pysselspel.tsx
+++ src/pages/Pysselspel.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { H1WhiteSecond } from "../components/styled/Fonts";
 import { BackgroundOriginal, CenteredColTransWrapper, WrapperTransparent } from "../components/styled/Wrappers";
-import Farglagg from '../assets/logos/Farglagg_logo2_300.png';
+import Farglagg from '../assets/logos/farglagg_logo2_300.png';
 import Memory from '../assets/logos/memory_logo300.png';
 //import JulMemory from '../assets/images/link_julmemory.png';
 import styled from "styled-components";
