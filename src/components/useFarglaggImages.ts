1d83fc0038a4cd1a5423414906833bb543938ffe
Carinalak <carina.lakosil@gmail.com>
2025-02-15 08:15:26 +0100

    Added more images

--- src/components/useFarglaggImages.ts
+++ src/components/useFarglaggImages.ts
@@ -3,23 +3,32 @@ import Kanin from '../assets/images/farglagg/kanin.jpg';
 import Apflicka from '../assets/images/farglagg/apflicka.jpg';
 import Hund from '../assets/images/farglagg/hund.jpg';
 import Nalle from '../assets/images/farglagg/nalle.jpg';
-/*import KaninMorotter from '../assets/images/farglagg/kanin_med_morotter.jpg';
+import KaninMorotter from '../assets/images/farglagg/kanin_med_morotter.jpg';
 import KyckilngAgg from '../assets/images/farglagg/kyckling_med_agg.jpg';
 import FagelHamster from '../assets/images/farglagg/fagel_hamster.jpg';
 import OrmHjarta from '../assets/images/farglagg/orm_hjarta.jpg';
 import GrisPol from '../assets/images/farglagg/gris_i_pol.jpg';
 import Hamster from '../assets/images/farglagg/hamster.jpg';
 import Fagel from '../assets/images/farglagg/fagel.jpg';
-import DrakeEld from '../assets/images/farglagg/drake_eld.jpg';*/
+import DrakeEld from '../assets/images/farglagg/drake_eld.jpg';
 
 
 export const useFarglaggImages = () => {
 
   return [
+    {src: DrakeEld, alt: "Eldsprutande drake" },
+    {src: KyckilngAgg, alt: "Kyckling med ägg" },
+    {src: KaninMorotter, alt: "Kanin med morötter" },
+    {src: OrmHjarta, alt: "Orm" },
+    {src: GrisPol, alt: "Gris i pöl" },
+    {src: Hamster, alt: "Hamster" },
+    {src: Fagel, alt: "Fågel" },
+    {src: FagelHamster, alt: "Fågel och hamster" },
     {src: Seal, alt: "Säl" },
-    {src: Kanin, alt: "Kanin" },
-    {src: Apflicka, alt: "Apflicka" },
     {src: Hund, alt: "Hund" },
     {src: Nalle, alt: "Nalle" },
+    {src: Kanin, alt: "Kanin" },
+    {src: Apflicka, alt: "Apflicka"},
+
   ];
 };
