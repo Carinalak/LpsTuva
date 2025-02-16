fd7f14cda5abaa2eff9ea08dc3f5800fbc45b784
Carinalak <carina.lakosil@gmail.com>
2025-02-16 09:18:26 +0100

    Added gallery images

--- src/components/useGalleryImages.ts
+++ src/components/useGalleryImages.ts
@@ -1,40 +1,72 @@
 import { useState, useEffect, useCallback } from "react";
-import BirdDarkPurple from "../assets/images/galleri/BirdDarkPurple.jpg";
-import AntPurple from "../assets/images/galleri/AntPurple.jpg";
-import BirdPink from "../assets/images/galleri/BirdPink.jpg";
-import BirdPink2 from "../assets/images/galleri/BirdPink2.jpg";
-import BirdPink3 from "../assets/images/galleri/BirdPink3.jpg";
-import RainbowCat from "../assets/images/galleri/CatPinkRainbow.jpg";
-import WhiteCat from "../assets/images/galleri/CatWhite.jpg";
-import DogWhitePurpleDot from "../assets/images/galleri/DogWhitePurpleDot.jpg";
-import DragonflyBlue from "../assets/images/galleri/DragonflyBlue.jpg";
-import IgelkottWhite from "../assets/images/galleri/IgelkottBrown.jpg";
-import GreyMouse from "../assets/images/galleri/MouseGrey.jpg";
+import { BREAKPOINT_TABLET } from "./styled/Variables";
+
+import BirdDarkPurple from "../assets/images/galleri/autumn/BirdDarkPurple.jpg";
+import AntPurple from "../assets/images/galleri/spring/AntPurple.jpg";
+import BrunKattKorsbar from "../assets/images/galleri/spring/brun_katt_korsbarsblommor.jpg";
+import FagelHyacinth from "../assets/images/galleri/spring/fagel_hyacinth.jpg";
+import GraHund from "../assets/images/galleri/spring/gra_hund_blomma.jpg";
+import KaninStubbe from "../assets/images/galleri/spring/kanin_stubbe.jpg";
+import LammGulKrokus from "../assets/images/galleri/spring/lamm_gul_krokus.jpg";
+import LilaKoKrokus from "../assets/images/galleri/spring/lila_ko_krokus.jpg";
+import VitKaninKrokus from "../assets/images/galleri/spring/vit_kanin_krokus.jpg";
+import BirdPink from "../assets/images/galleri/summer/BirdPink.jpg";
+import BirdPink2 from "../assets/images/galleri/summer/BirdPink2.jpg";
+import BirdPink3 from "../assets/images/galleri/summer/BirdPink3.jpg";
+import RainbowCat from "../assets/images/galleri/spring/CatPinkRainbow.jpg";
+import WhiteCat from "../assets/images/galleri/autumn/CatWhite.jpg";
+import DogWhitePurpleDot from "../assets/images/galleri/summer/DogWhitePurpleDot.jpg";
+import DragonflyBlue from "../assets/images/galleri/summer/DragonflyBlue.jpg";
+import IgelkottWhite from "../assets/images/galleri/autumn/IgelkottBrown.jpg";
+import GreyMouse from "../assets/images/galleri/autumn/MouseGrey.jpg";
 import WinterPinkBear from "../assets/images/galleri/winter/WinterPinkBear.jpg";
-import SnowKana from "../assets/images/galleri/winter/Snow_kana.jpg";
 import SealOnIce from "../assets/images/galleri/winter/SealOnIce.jpg";
 import NalleLilaMossa from "../assets/images/galleri/winter/NalleLilaMossa.jpg";
 import CrystalBear from "../assets/images/galleri/winter/IceBearSnowCrystals.jpg";
-import { BREAKPOINT_TABLET } from "./styled/Variables";
+import HundKalkeSnow1 from "../assets/images/galleri/winter/hundar_kalke_snow.jpg";
+import HundKalkeSnow2 from "../assets/images/galleri/winter/hundar_kalke_snow2.jpg";
+import LilaKaninSnow from "../assets/images/galleri/winter/lila_kanin_snow.jpg";
+import RosaKatt from "../assets/images/galleri/winter/rosa_katt.jpg";
+import SnowKana from "../assets/images/galleri/winter/Snow_kana.jpg";
+import RavMossaSnow from "../assets/images/galleri/winter/Rav_mossa_snow.jpg";
+import ValrossIs from "../assets/images/galleri/winter/valross_is.jpg";
+import KaninPulka1 from "../assets/images/galleri/winter/kanin_pulka.jpg";
+import KaninPulka2 from "../assets/images/galleri/winter/kanin_pulka2.jpg";
+
 
 const images = [
+  { src: CrystalBear, alt: "Björn med iskristaller", season: "winter" },
+  { src: RosaKatt, alt: "Rosa katt", season: "winter" },
+  
+  { src: HundKalkeSnow1, alt: "Hundar på kälke", season: "winter" },
+  { src: HundKalkeSnow2, alt: "Hundar på kälke", season: "winter" },
   { src: BirdDarkPurple, alt: "Purple Bird", season: "autumn" },
   { src: AntPurple, alt: "Purple Ant", season: "spring" },
   { src: BirdPink, alt: "Pink Bird", season: "summer" },
   { src: BirdPink2, alt: "Pink Bird second", season: "summer" },
   { src: BirdPink3, alt: "Purple Bird third", season: "summer" },
+  { src: LilaKoKrokus, alt: "Lila ko bredvid lila krokus", season: "spring" },
   { src: RainbowCat, alt: "Rainbowcat", season: "spring" },
+  { src: LammGulKrokus, alt: "Gulligt lamm bredvid en vit krokus", season: "spring" },
+  { src: FagelHyacinth, alt: "En fågel bredvid en hyacint", season: "spring" },
+  { src: BrunKattKorsbar, alt: "En brun katt bredvid körsbärsblommor", season: "spring" },
   { src: WhiteCat, alt: "White cat", season: "autumn" },
+  { src: GraHund, alt: "Grå hund", season: "spring" },
+  { src: KaninStubbe, alt: "En kanin som sitter på en stubbe", season: "spring" },
+  { src: VitKaninKrokus, alt: "En vit kanin bredvid en krokus", season: "spring" },
   { src: DogWhitePurpleDot, alt: "Dog with dots", season: "autumn" },
   { src: DragonflyBlue, alt: "Blue dragonfly", season: "summer" },
-  { src: IgelkottWhite, alt: "Hedgehog", season: "autumn" },
-  { src: GreyMouse, alt: "Grey Mouse", season: "autumn" },
-  { src: SnowKana, alt: "Snökana", season: "winter" },
-  { src: WinterPinkBear, alt: "Rosa nalle", season: "winter" },
+  { src: IgelkottWhite, alt: "En igelkotte", season: "autumn" },
+  { src: GreyMouse, alt: "en grå mus", season: "autumn" },
   { src: SealOnIce, alt: "En säl på isen", season: "winter" },
   { src: NalleLilaMossa, alt: "Nalle med lila mössa", season: "winter" },
-  { src: CrystalBear, alt: "Björn med iskristaller", season: "winter" },
-  
+  { src: LilaKaninSnow, alt: "Lila kanin i snö", season: "winter" },
+  { src: SnowKana, alt: "Snökana", season: "winter" },
+  { src: RavMossaSnow, alt: "Räv i mossa", season: "winter" },
+  { src: ValrossIs, alt: "Valross i is", season: "winter" },
+  { src: KaninPulka1, alt: "Kanin på pulka", season: "winter" },
+  { src: KaninPulka2, alt: "Kanin på pulka", season: "winter" },
+  { src: WinterPinkBear, alt: "Rosa nalle", season: "winter" },
 ];
 
 export const useGalleryImages = (sortBy: string) => {
