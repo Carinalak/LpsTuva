390e147ccb4254bd267d9257ff86b57cb8eb234e
Carinalak <carina.lakosil@gmail.com>
2025-10-23 08:11:19 +0200

    Webmaster link and other

--- src/pages/Webmaster.tsx
+++ src/pages/Webmaster.tsx
@@ -10,8 +10,7 @@ export const Webmaster = () => {
             <div>
             Vad vill du se mer av på den här sidan? Är det något du vill ändra på, eller något som ser konstigt ut 
             eller inte fungerar? Maila till 
-            Webmaster <StyledLink to="mailto:carina.lakosil@gmail.com" target="_blank" rel="noopener noreferrer"> Carina </StyledLink>
-            så ska hon svara på dina frågor.</div>
+            Webmaster <StyledLink to="mailto:carina.lakosil@gmail.com" target="_blank" rel="noopener noreferrer"> Carina</StyledLink>.</div>
             
         </WhiteWrapperMini>
 </>
