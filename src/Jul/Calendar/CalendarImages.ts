6ac3a655695cf1a8d77e8b2850e0c6387dcc2877
Carinalak <carina.lakosil@gmail.com>
2025-10-30 13:45:23 +0100

    Christmas calendar-fixings

--- src/Jul/Calendar/CalendarImages.ts
+++ src/Jul/Calendar/CalendarImages.ts
@@ -1,91 +1,52 @@
-
-  //import SeerIngetAnim from "../../Jul/Calendar/SeerInget_snoboll/SeerInget_snoboll_animation.gif"
-  // 🔹 Importera dina lokala bilder
-  import BunnyRedLeaf from "../../assets/images/galleri/autumn/BunnyRedLeaf.jpg";
-  import HundOrangeLov from "../../assets/images/galleri/autumn/Hund_orange_lov.jpg";
-  import Gungar from "../../assets/images/galleri/autumn/Gungar.jpg";
-  import SpindelOrangeLov1 from "../../assets/images/galleri/autumn/Spindel_orangelov1.jpg";
-  import RosaKatt from "../../assets/images/galleri/winter/rosa_katt.jpg";
-  import SnowKana from "../../assets/images/galleri/winter/Snow_kana.jpg";
-  import RavMossaSnow from "../../assets/images/galleri/winter/Rav_mossa_snow.jpg";
-  import ValrossIs from "../../assets/images/galleri/winter/valross_is.jpg";
-  import KaninPulka1 from "../../assets/images/galleri/winter/kanin_pulka.jpg";
-  import KaninPulka2 from "../../assets/images/galleri/winter/kanin_pulka2.jpg";
-  import SeerInget_1 from "../../Jul/Calendar/SeerInget_snoboll/SeerInget_snoboll_1.png";
+import BunnyRedLeaf from "../../assets/images/galleri/autumn/BunnyRedLeaf.jpg";
+import HundOrangeLov from "../../assets/images/galleri/autumn/Hund_orange_lov.jpg";
+import Gungar from "../../assets/images/galleri/autumn/Gungar.jpg";
+import SpindelOrangeLov1 from "../../assets/images/galleri/autumn/Spindel_orangelov1.jpg";
+import RosaKatt from "../../assets/images/galleri/winter/rosa_katt.jpg";
+import SnowKana from "../../assets/images/galleri/winter/Snow_kana.jpg";
+import RavMossaSnow from "../../assets/images/galleri/winter/Rav_mossa_snow.jpg";
+import ValrossIs from "../../assets/images/galleri/winter/valross_is.jpg";
+import KaninPulka1 from "../../assets/images/galleri/winter/kanin_pulka.jpg";
+import KaninPulka2 from "../../assets/images/galleri/winter/kanin_pulka2.jpg";
+import SeerInget_1 from "../../Jul/Calendar/SeerInget_snoboll/SeerInget_snoboll_1.png";
 import SeerInget_2 from "../../Jul/Calendar/SeerInget_snoboll/SeerInget_snoboll_2-3.png";
 import SeerInget_3 from "../../Jul/Calendar/SeerInget_snoboll/SeerInget_snoboll_4.png";
 import SeerInget_4 from "../../Jul/Calendar/SeerInget_snoboll/SeerInget_snoboll_5.png";
 
 
-    // 🔹 Bilder med hårdkodat lucknummer
-  export const CalendarImages = [
-
+//  Bilder med hårdkodat lucknummer
+export const CalendarImages = [
 
   {
     number: 1,
     gallery: [
-      { src: SeerInget_1, alt: "Jag seer ingenting!" },
-      { src: SeerInget_2, alt: "Jag seer inget 2" },
-      { src: SeerInget_3, alt: "Jag seer inget 3" },
-      { src: SeerInget_4, alt: "Jag seer inget 4" },
+      { src: SeerInget_1, alt: "Tomte med randig luva säger: Jag seer ingenting!" },
+      { src: SeerInget_2, alt: "Tomte med prickig luva säger: Inte jag heller, men jag kände att jag fick nåt kallt i ansiktet. Första tomten säger: Men du har ju inget ansikte!" },
+      { src: SeerInget_3, alt: "Tomte med prickig luva säger: Hur kan du veta att jag inte har nåt ansikte när du inte kan se mig?" },
+      { src: SeerInget_4, alt: "Tomte med prickig luva fortsätter: Det var säkert du som kastade den där snöbollen på mig." },
     ],
   },
-    { src: RosaKatt, alt: "Rosa katt", number: 2 },
-    { src: BunnyRedLeaf, alt: "Kanin med ett rött löv", number: 3 },
-    { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 4 },
-    { src: HundOrangeLov, alt: "Hund bland orange löv", number: 5 },
-    { src: SnowKana, alt: "Snökana", number: 6 },
-    { src: ValrossIs, alt: "Valross i is", number: 7 },
-    { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 8 },
-    { src: KaninPulka1, alt: "Kanin på pulka", number: 9 },
-    { src: KaninPulka2, alt: "Kanin på pulka", number: 10 },
-    { src: BunnyRedLeaf, alt: "Kanin med ett rött löv", number: 11 },
-    { src: HundOrangeLov, alt: "Hund bland orange löv", number: 12 },
-    { src: Gungar, alt: "Petshoparna gungar på en gul gunga", number: 13 },
-    { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 14 },
-    { src: RavMossaSnow, alt: "Räv i mossa", number: 15 },
-    { src: SnowKana, alt: "Snökana", number: 16 },
-    { src: RavMossaSnow, alt: "Räv i mossa", number: 17 },
-    { src: ValrossIs, alt: "Valross i is", number: 18 },
-    { src: KaninPulka1, alt: "Kanin på pulka", number: 19 },
-    { src: KaninPulka2, alt: "Kanin på pulka", number: 20 },
-    { src: BunnyRedLeaf, alt: "Kanin med ett rött löv", number: 21 },
-    { src: HundOrangeLov, alt: "Hund bland orange löv", number: 22 },
-    { src: Gungar, alt: "Petshoparna gungar på en gul gunga", number: 23 },
-    { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 24 },
-  ];
-
-  /*
-
-    { 
-      slug: "SeerInget_1", 
-      src: SeerInget_1, 
-      name: "Jag seer inget1", 
-
-      alt: "Vision of Life",
-    },
-    { 
-      slug: "SeerInget_2", 
-      src: SeerInget_2, 
-      name: "Jag seer inget2", 
-      alt: "Sweet Alligator",
-
-    },
-    { 
-      slug: "SeerInget_3", 
-      src: SeerInget_3, 
-      name: "Jag seer inget3", 
-      alt: "Jag seer inget3",
-      date: { day: 15, month: "jun" },
-    },
-    { 
-      slug: "SeerInget_4", 
-      src: SeerInget_4, 
-      name: "Jag seer inget14", 
-
-      alt: "Jag seer inget4",
-    },
-
-  ];
-
-  */
+  { src: RosaKatt, alt: "Rosa katt", number: 2 },
+  { src: BunnyRedLeaf, alt: "Kanin med ett rött löv", number: 3 },
+  { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 4 },
+  { src: HundOrangeLov, alt: "Hund bland orange löv", number: 5 },
+  { src: SnowKana, alt: "Snökana", number: 6 },
+  { src: ValrossIs, alt: "Valross i is", number: 7 },
+  { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 8 },
+  { src: KaninPulka1, alt: "Kanin på pulka", number: 9 },
+  { src: KaninPulka2, alt: "Kanin på pulka", number: 10 },
+  { src: BunnyRedLeaf, alt: "Kanin med ett rött löv", number: 11 },
+  { src: HundOrangeLov, alt: "Hund bland orange löv", number: 12 },
+  { src: Gungar, alt: "Petshoparna gungar på en gul gunga", number: 13 },
+  { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 14 },
+  { src: RavMossaSnow, alt: "Räv i mossa", number: 15 },
+  { src: SnowKana, alt: "Snökana", number: 16 },
+  { src: RavMossaSnow, alt: "Räv i mossa", number: 17 },
+  { src: ValrossIs, alt: "Valross i is", number: 18 },
+  { src: KaninPulka1, alt: "Kanin på pulka", number: 19 },
+  { src: KaninPulka2, alt: "Kanin på pulka", number: 20 },
+  { src: BunnyRedLeaf, alt: "Kanin med ett rött löv", number: 21 },
+  { src: HundOrangeLov, alt: "Hund bland orange löv", number: 22 },
+  { src: Gungar, alt: "Petshoparna gungar på en gul gunga", number: 23 },
+  { src: SpindelOrangeLov1, alt: "Spindel på gren", number: 24 },
+];
